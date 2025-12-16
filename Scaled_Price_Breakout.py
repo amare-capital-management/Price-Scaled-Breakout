556f5b28e9d751582ff280f31d4d7edaede01f3b
ACM <kabelojunior10@gmail.com>
2025-12-16 09:16:04 +0200

    Update Scaled_Price_Breakout.py

--- Scaled_Price_Breakout.py
+++ Scaled_Price_Breakout.py
@@ -9,7 +9,40 @@ from datetime import datetime
 # -------------------------------------
 # CONFIG
 # -------------------------------------
-tickers = []
+tickers = ["ABN.AS", "ADYEN.AS", "ASML.AS", "HEIA.AS", "JDEP.AS", "PHIA.AS", "RAND.AS", "SHELL.AS", "TKWY.AS", "UNA.AS", "WKL.AS", "ACR1.F", 
+           "XCA.F", "AIR.F", "AXA.F", "BSN.F", "BNP.F", "CAR1.F", "CAJ1.F", "GZFB.F", "76B.F", "LOR.F", "LVMHF", "MCHA.F", "MOS.F", "RNL.F", 
+           "TH1.F", "VACE.F", "ADS.DE", "ALV.DE", "BAS.DE", "BAYN.DE", "BEI.DE", "BMW.DE", "CBK.DE", "CON.DE", "1CO.DE", "DBK.DE", "DB1.DE", 
+           "DHL.DE", "DTE.DE", "EOAN.DE", "FME.DE", "HEI.DE", "IFX.DE", "LHA.DE", "PAH3.DE", "PUM.DE", "RWE.DE", "SAP.DE", "SIE.DE", "VOW3.DE", 
+           "VNA.DE", "ACB.TO", "CM.TO", "BMO.TO", "ABX.TO", "BCE.TO", "BNS.TO", "CNR.TO", "WEED.TO", "ENB.TO", "GWO.TO", "MFC.TO", "CNQ.TO",
+           "NTR.TO", "CP.TO", "RY.TO", "SHOP.TO", "SU.TO", "TRP.TO", "TD.TO", "T.TO", "WCN.TO", "MMM", "ABT", "ABBV", "ABC", "ABMD", "ACN",
+           "ADM", "ADBE", "ADP", "AAP", "AEP", "AES", "AFL", "AIG", "ABNB", "AIV", "AJG", "ALK", "BABA", "ALGN", "ALLE", "LNT", "ALL", "MO",
+           "AMAT", "AMZN", "AMC", "AMD", "DOX", "AEE", "AAL", "AMT", "EXP", "AME", "AMG", "AMGN", "AMP", "APH", "ADI", "ANSS", "AON", "APA",
+           "APD", "AAPL", "APTV", "ARNC", "ARE", "ANET", "ARM", "ASML", "AIZ", "T", "ATO", "ADSK", "AZO", "AVB", "AVY", "AWK", "BIDU", "BHGE",
+           "USB", "BHC", "BAX", "BDX", "BRK-B", "BBY", "BYND", "BILI", "BIIB", "BMRN", "BLK", "BK", "BAC", "BA", "BKNG", "BWA", "BXP", "BSX",
+           "BMY", "AVGO", "BR", "CDNS", "CCJ", "CPB", "CGC", "COF", "CPRI", "CAH", "KMX", "CCL", "CAT", "CBOE", "CBRE", "CE", "CNC", "CNP",
+           "CF", "SCHW", "CHTR", "CHD", "CVX", "LFC", "ZNH", "CMG", "CHRW", "CB", "CI", "CINF", "CTAS", "CSCO", "C", "CFG", "CTXS", "CLX", 
+           "CME", "CMS", "CCE", "COIN", "CL", "CMCSA", "CMA", "CAG", "COP", "COO", "CPRT", "GLW", "COST", "COTY", "CCI", "CSX", "CVS", "DHR",
+           "DRI", "DVA", "DAL", "DVN", "DFS", "FANG", "WBD", "DISH", "DIS", "DLR", "DG", "DLTR", "D", "DPZ", "DOV", "DOW", "DHI", "DUK", "DRE",
+           "DXC", "DVAX", "EA", "EMN", "ETN", "EBAY", "ECL", "ED", "EW", "LLY", "EMR", "ETR", "EOG", "EQR", "EFX", "EQIX", "ESS", "EL", "EVRG",
+           "ES", "EXC", "EXPD", "EXPE", "EXR", "XOM", "META", "FAST", "FDX", "FFIV", "FIS", "FE", "FISV", "FITB", "FLT", "FLS", "FLR", "FMX",
+           "F", "FWONA", "FTNT", "FBHS", "FOXA", "BEN", "FCX", "FRT", "GRMN", "IT", "GD", "GDS", "GE", "GIS", "GMAB", "GPC", "GILD", "GPN",
+           "GM", "GOOG", "GRPN", "GS", "HAL", "HBI", "HOG", "LHX", "HAS", "HBAN", "HCA", "PEAK", "HP", "HSCI", "HSY", "HES", "HPE", "HIG", 
+           "HLT", "HOLX", "HD", "HON", "HRL", "HPQ", "HRB", "HST", "HTHT", "HUM", "IBM", "ICE", "IDXX", "IFF", "ILMN", "INCY", "INO", "INTC",
+           "IP", "IPG", "INTU", "ISRG", "IVZ", "NVTA", "IPGP", "IQV", "IRM", "ITW", "JKHY", "JD", "JEF", "SJM", "JNJ", "DE", "YY", "JPM", "JNPR",
+           "K", "KEY", "KEYS", "KMB", "KIM", "KMI", "KLAC", "KSS", "KHC", "KR", "LW", "LEG", "LEN", "LEVI", "LBTY", "LNC", "LIN", "LKQ", "LMT",
+           "LOW", "LRCX", "LULU", "LYFT", "MAA", "MAC", "M", "MANU", "MPC", "MRO", "MAR", "MAS", "MA", "MAT", "MKC", "MCD", "MCK", "MDT", "MELI",
+           "MRK", "META", "MET", "MTD", "MGM", "MCHP", "MU", "MSFT", "MLM", "MMC", "MRNA", "MHK", "TAP", "MOMO", "MDLZ", "MNST", "MCO", "MS",
+           "MOS", "MSI", "MSCI", "MTB", "NDAQ", "NOV", "NKTR", "NTAP", "NTES", "NFLX", "NWL", "NEM", "EDU", "NWSA", "NEE", "NLSN", "NKE", "NIO",
+           "NI", "JWN", "NSC", "NTRS", "NOC", "NLOC", "NCLH", "NVO", "NRG", "NUE", "NVDA", "OXY", "OMC", "OKE", "ORCL", "ORLY", "PCAR", "PZZA",
+           "PH", "PAYX", "PYPL", "PEG", "PTON", "PNR", "PEP", "PKI", "PFG", "PFE", "PG", "PM", "PSX", "PDD", "PNW", "PINS", "PKG", "PNC", "PPG",
+           "PPL", "PGR", "PLD", "PRU", "PHM", "PVH", "PXD", "QRVO", "QCOM", "PWR", "DGX", "RL", "RJF", "RTX", "O", "RDDT", "REG", "REGN", "RMD",
+           "RF", "RHI", "HOOD", "ROK", "ROL", "ROP", "ROST", "RCL", "SPGI", "CRM", "SBAC", "SLB", "STX", "SEE", "SRE", "SHAK", "SHW", "SPG",
+           "SIRI", "XRAY", "SWKS", "SLG", "AOS", "SNAP", "SONY", "SO", "LUV", "SWK", "SBUX", "STT", "SYK", "SIVB", "SYF", "SNPS", "SYY", "TMUS",
+           "TTWO", "TAL", "TPR", "TGT", "FTI", "TEL", "TFX", "TME", "TSLA", "TEVA", "TXN", "TXT", "GAP", "TMO", "TJX", "TKO", "TSCO", "TDG", "TRV",
+           "TCOM", "TRIP", "TROW", "TFC", "TSN", "UBER", "UDR", "ULTA", "UUA", "UNP", "UAL", "UNH", "UPS", "URI", "UNM", "VLO", "VTR", "VRSK", 
+           "VZ", "VRSN", "VRTX", "VFC", "VIPS", "VIR", "SPCE", "V", "VNO", "VMC", "WAB", "WMT", "WM", "WAT", "WEC", "WB", "WFC", "WELL", "WDC",
+           "WU", "WRK", "WY", "WHR", "WMB", "WDAY", "WWE", "GWW", "WYNN", "XEL", "XRX", "XYL", "YUM", "YUMC", "ZBH", "ZION", "ZTS", "ZM", "ZTO"]
+
 start_date = "2025-01-01"
 end_date = datetime.today().strftime("%Y-%m-%d")
 output_dir = "breakout_plots_pro"
@@ -130,4 +163,5 @@ for ticker in tickers:
     print(f"  → Saved {filename}\n")
 
 print("All charts generated successfully!")
+
 print(f"Folder: {os.path.abspath(output_dir)}")
